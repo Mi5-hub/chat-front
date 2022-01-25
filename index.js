5dfe6f9f88db4c060ebe442c7ce6cf33313bc834
Mi5-hub <mino.randimbiarilanto@gmail.com>
2022-01-25 17:01:38 +0300

    test

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: 'https://chat-server-fb.herokuapp.com/',
+        origin: 'https://chat-fb-client.herokuapp.com/',
         methods: ['GET', 'POST'],
     }
 });
